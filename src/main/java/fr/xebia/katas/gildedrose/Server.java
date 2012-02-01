7d6f971b61a389f240e866477803a37416d47604
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2012-02-01 20:56:33 +0100

    Stable with web

---
+++ src/main/java/fr/xebia/katas/gildedrose/Server.java
@@ -0,0 +1,59 @@
+package fr.xebia.katas.gildedrose;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.mortbay.jetty.handler.AbstractHandler;
+
+public class Server {
+
+    final Inn inn;
+
+    Server() {
+        inn = new Inn();
+    }
+
+    public String handle() {
+        StringBuilder out = new StringBuilder();
+        out.append("<h1>Inn</h1>");
+        out.append("<table><tr><th>Name</th><th>Sell in</th><th>Quality</th></tr>");
+        for (Item item : inn.getItems()) {
+            out.append("<tr>");
+            out.append("<td>").append(item.getName()).append("</td>");
+            out.append("<td>").append(item.getSellIn()).append("</td>");
+            out.append("<td>").append(item.getQuality()).append("</td>");
+            out.append("</tr>");
+        }
+        out.append("</table>");
+        out.append("<form method=\"post\">");
+        out.append("<input type=\"submit\" value=\"increaseQuality\">");
+        out.append("</form>");
+        return out.toString();
+    }
+
+    public static void main(String[] args) throws Throwable {
+        org.mortbay.jetty.Server server = new org.mortbay.jetty.Server(8080);
+        final Server myServer = new Server();
+
+        server.addHandler(new AbstractHandler() {
+            @Override
+            public void handle(String arg0, HttpServletRequest arg1, HttpServletResponse response, int arg3)
+                    throws IOException, ServletException {
+                response.setContentType("text/html;charset=utf-8");
+                response.setStatus(HttpServletResponse.SC_OK);
+                if ("POST".equals(arg1.getMethod())) {
+                    myServer.inn.updateQuality();
+                }
+                response.getWriter().println(myServer.handle());
+                response.getWriter().flush();
+            }
+        });
+
+        server.start();
+        server.join();
+    }
+
+}
--- src/test/java/fr/xebia/katas/gildedrose/InnTest.java
+++ src/test/java/fr/xebia/katas/gildedrose/InnTest.java
@@ -1,14 +1,124 @@
 package fr.xebia.katas.gildedrose;
 
-import org.junit.Test;
-
 import static org.fest.assertions.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
 
 public class InnTest {
 
-   @Test
-   public void should_test_the_truth() throws Exception {
-      assertThat(true).isTrue();
-   }
+    @Test
+    public void should_test_the_truth() throws Exception {
+        assertThat(true).isTrue();
+    }
+
+    @Test
+    public void should_get_items() throws Exception {
+        Inn inn = new Inn();
+        List<Item> items = inn.getItems();
+
+        assertThat(items).hasSize(6);
+        assertItem(items.get(0), "+5 Dexterity Vest", 10, 20);
+        assertItem(items.get(1), "Aged Brie", 2, 0);
+        assertItem(items.get(2), "Elixir of the Mongoose", 5, 7);
+        assertItem(items.get(3), "Sulfuras, Hand of Ragnaros", 0, 80);
+        assertItem(items.get(4), "Backstage passes to a TAFKAL80ETC concert", 15, 20);
+        assertItem(items.get(5), "Conjured Mana Cake", 3, 6);
+
+        inn.updateQuality();
+
+        assertThat(items).hasSize(6);
+        assertItem(items.get(0), "+5 Dexterity Vest", 9, 19);
+        assertItem(items.get(1), "Aged Brie", 1, 1);
+        assertItem(items.get(2), "Elixir of the Mongoose", 4, 6);
+        assertItem(items.get(3), "Sulfuras, Hand of Ragnaros", 0, 80);
+        assertItem(items.get(4), "Backstage passes to a TAFKAL80ETC concert", 14, 21);
+        assertItem(items.get(5), "Conjured Mana Cake", 2, 4);
+
+        inn.updateQuality();
+
+        assertThat(items).hasSize(6);
+        assertItem(items.get(0), "+5 Dexterity Vest", 8, 18);
+        assertItem(items.get(1), "Aged Brie", 0, 2);
+        assertItem(items.get(2), "Elixir of the Mongoose", 3, 5);
+        assertItem(items.get(3), "Sulfuras, Hand of Ragnaros", 0, 80);
+        assertItem(items.get(4), "Backstage passes to a TAFKAL80ETC concert", 13, 22);
+        assertItem(items.get(5), "Conjured Mana Cake", 1, 2);
+    }
+
+    private Inn singleItem(Item item) {
+        Inn inn = new Inn();
+        List<Item> items = inn.getItems();
+        items.clear();
+        items.add(item);
+        return inn;
+    }
+
+    @Test
+    public void with_backstage_10_days_quality_increase_by_2() throws Exception {
+        Item backstageItem = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10);
+        Inn inn = singleItem(backstageItem);
+
+        inn.updateQuality();
+
+        assertItem(backstageItem, "Backstage passes to a TAFKAL80ETC concert", 9, 12);
+    }
+
+    @Test
+    public void with_backstage_5_days_quality_increase_by_3() throws Exception {
+        Item backstageItem = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10);
+        Inn inn = singleItem(backstageItem);
+
+        inn.updateQuality();
+
+        assertItem(backstageItem, "Backstage passes to a TAFKAL80ETC concert", 4, 13);
+    }
+
+    @Test
+    public void should_decrease_twice_once_sell_date_is_passed() throws Exception {
+        Item dummyItem = new Item("dummy", -1, 10);
+        Inn inn = singleItem(dummyItem);
+
+        inn.updateQuality();
+
+        assertItem(dummyItem, "dummy", -2, 8);
+    }
+
+    @Test
+    public void with_passed_backstaged_should_have_quality_zero() throws Exception {
+        Item backstageItem = new Item("Backstage passes to a TAFKAL80ETC concert", -1, 10);
+        Inn inn = singleItem(backstageItem);
+
+        inn.updateQuality();
+
+        assertItem(backstageItem, "Backstage passes to a TAFKAL80ETC concert", -2, 0);
+    }
+
+    @Test
+    public void with_passed_aged_brie_should_increase_quality_twice() throws Exception {
+        Item backstageItem = new Item("Aged Brie", -1, 10);
+        Inn inn = singleItem(backstageItem);
+
+        inn.updateQuality();
+
+        assertItem(backstageItem, "Aged Brie", -2, 12);
+    }
+
+    @Test
+    public void with_conjured_quality_degrades_twice() throws Exception {
+        Item conjuredItem = new Item("Conjured", 10, 42);
+        Inn inn = singleItem(conjuredItem);
+
+        inn.updateQuality();
+
+        assertItem(conjuredItem, "Conjured", 9, 40);
+    }
+
+    private void assertItem(Item item, String expectedName, int expectedSellIn, int exptectedQuality) {
+        assertThat(item.getName()).isEqualTo(expectedName);
+        assertThat(item.getSellIn()).isEqualTo(expectedSellIn);
+        assertThat(item.getQuality()).isEqualTo(exptectedQuality);
+    }
 
 }
