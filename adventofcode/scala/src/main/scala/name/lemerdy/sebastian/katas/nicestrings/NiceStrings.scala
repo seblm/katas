9b79fcfa5042f68fc60d46402040a9609a9180c7
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2015-12-21 18:18:13 +0100

    Day 5 Part One

--- adventofcode/scala/src/main/scala/name/lemerdy/sebastian/katas/nicestrings/NiceStrings.scala
+++ adventofcode/scala/src/main/scala/name/lemerdy/sebastian/katas/nicestrings/NiceStrings.scala
@@ -1,8 +1,40 @@
 package name.lemerdy.sebastian.katas.nicestrings
 
-/**
-  * Created by seb on 21/12/2015.
-  */
-class NiceStrings {
+import scala.io.Source
 
+class NiceStrings {
+  def isNice(value: String): Boolean = {
+    Seq(Vowels, TwiceLetter, ForbiddenPatterns).forall(_.isNice(value))
+  }
+}
+
+trait IsNice {
+  def isNice(value: String): Boolean
+}
+
+object Vowels extends IsNice {
+  override def isNice(value: String): Boolean = isNice(value.toList, 0)
+
+  def isNice(value: List[Char], vowels: Int): Boolean = value match {
+    case Nil => vowels >= 3
+    case head :: tail => isNice(tail, vowels + (if ("aeiou".contains(head)) 1 else 0))
+  }
+}
+
+object TwiceLetter extends IsNice {
+  override def isNice(value: String) = "([a-z])\\1".r.findFirstIn(value).isDefined
+}
+
+object ForbiddenPatterns extends IsNice {
+  override def isNice(value: String): Boolean = "(ab|cd|pq|xy)".r.findFirstIn(value).isEmpty
+}
+
+object NiceStrings {
+  def main(args: Array[String]) {
+    val niceStrings: NiceStrings = new NiceStrings()
+    println(Source
+      .fromInputStream(getClass.getResourceAsStream("input"))
+      .getLines()
+      .count(niceStrings.isNice))
+  }
 }
--- adventofcode/scala/src/test/scala/name/lemerdy/sebastian/katas/nicestrings/NiceStringsSpec.scala
+++ adventofcode/scala/src/test/scala/name/lemerdy/sebastian/katas/nicestrings/NiceStringsSpec.scala
@@ -1,8 +1,18 @@
 package name.lemerdy.sebastian.katas.nicestrings
 
-/**
-  * Created by seb on 21/12/2015.
-  */
-class NiceStringsSpec {
+import org.scalatest.{FlatSpec, Matchers}
+
+class NiceStringsSpec extends FlatSpec with Matchers {
+
+  "Santa" should "tell if a string is nice" in {
+    new NiceStrings().isNice("ugknbfddgicrmopn") shouldBe true
+    new NiceStrings().isNice("aaa") shouldBe true
+  }
+
+  it should "tell if a string is naughty" in {
+    new NiceStrings().isNice("jchzalrnumimnmhp") shouldBe false
+    new NiceStrings().isNice("haegwjzuvuyypxyu") shouldBe false
+    new NiceStrings().isNice("dvszwmarrgswjxmb") shouldBe false
+  }
 
 }
