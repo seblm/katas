42ffd83c36004b0b5eb81e746ae89c47c6a46f21
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2015-12-23 06:36:02 +0100

    Day 6 Part One

---
+++ adventofcode/scala/src/main/scala/name/lemerdy/sebastian/katas/lights/Lights.scala
@@ -0,0 +1,60 @@
+package name.lemerdy.sebastian.katas.lights
+
+import scala.io.Source
+import scala.util.matching.Regex
+
+class Lights {
+
+  private val lights: Array[Boolean] = new Array[Boolean](1000000)
+
+  def input(specification: String): Lights = {
+    val parsers = Map[Regex, (Int, Int, Int, Int) => Lights](
+      ("turn on (\\d+),(\\d+) through (\\d+),(\\d+)".r, turnOn),
+      ("toggle (\\d+),(\\d+) through (\\d+),(\\d+)".r, toggle),
+      ("turn off (\\d+),(\\d+) through (\\d+),(\\d+)".r, turnOff)
+    )
+    parsers.flatMap({
+      case (regex, f) =>
+        regex.findFirstMatchIn(specification)
+          .map(m => f(m.group(1).toInt, m.group(2).toInt, m.group(3).toInt, m.group(4).toInt))
+    }).headOption.getOrElse(this)
+  }
+
+  def turnOn(x1: Int, y1: Int, x2: Int, y2: Int): Lights =
+    forEachLight(x1, y1, x2, y2, _ => true)
+
+  def toggle(x1: Int, y1: Int, x2: Int, y2: Int): Lights =
+    forEachLight(x1, y1, x2, y2, lightState => !lightState)
+
+  def turnOff(x1: Int, y1: Int, x2: Int, y2: Int): Lights =
+    forEachLight(x1, y1, x2, y2, _ => false)
+
+  def count(): Int = {
+    var lightsOn = 0
+    forEachLight(0, 0, 999, 999, lightState => {
+      if (lightState) lightsOn += 1
+      lightState
+    })
+    lightsOn
+  }
+
+  private def forEachLight(x1: Int, y1: Int, x2: Int, y2: Int, newLightState: Boolean => Boolean): Lights = {
+    for (x <- Stream.range(x1, x2 + 1))
+      for (y <- Stream.range(y1, y2 + 1)) {
+        val index: Int = y * 1000 + x
+        val oldLightState = lights(index)
+        lights(index) = newLightState(oldLightState)
+      }
+    this
+  }
+}
+
+object Lights {
+  def main(args: Array[String]) {
+    val lights = new Lights
+    Source.fromInputStream(getClass.getResourceAsStream("input"))
+      .getLines()
+      .foreach(lights.input)
+    println(s"number of lights: ${lights.count()}")
+  }
+}
---
+++ adventofcode/scala/src/test/scala/name/lemerdy/sebastian/katas/lights/LightsSpec.scala
@@ -0,0 +1,35 @@
+package name.lemerdy.sebastian.katas.lights
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class LightsSpec extends FlatSpec with Matchers {
+
+  "lights" should "be all turn on" in {
+    new Lights()
+      .turnOn(0, 0, 999, 999)
+      .count() shouldBe 1000000
+  }
+
+  it should "be toggled" in {
+    new Lights()
+      .turnOn(0, 0, 999, 999)
+      .toggle(0, 0, 999, 0)
+      .count() shouldBe 1000000 - 1000
+  }
+
+  it should "be turned off" in {
+    new Lights()
+      .turnOn(0, 0, 999, 999)
+      .turnOff(499, 499, 500, 500)
+      .count() shouldBe 1000000 - 4
+  }
+
+  it should "parse input" in {
+    new Lights()
+      .input("turn on 0,0 through 999,999")
+      .input("toggle 0,0 through 999,0")
+      .input("turn off 499,499 through 500,500")
+      .count() shouldBe 1000000 - 1000 - 4
+  }
+
+}
