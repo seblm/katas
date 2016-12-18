ad9639a1f8a8a74964bf7be92dd067f4f3f754ad
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2016-12-18 14:27:17 +0100

    Remove StringBuilder from parameters
    This is more functional like this.

--- diamond/src/main/scala/diamond/Diamond.scala
+++ diamond/src/main/scala/diamond/Diamond.scala
@@ -1,19 +1,22 @@
 package diamond
 
+import scala.collection.mutable.StringBuilder.newBuilder
+
 case class Diamond(size: Int) {
   def print(): String = {
-    val print = new StringBuilder
+    val print = newBuilder
     for (y <- size / 2 to 0 by -1) {
-      printLine(print, y)
+      print.append(printLine(y))
     }
     for (y <- 1 to size / 2) {
-      printLine(print, y)
+      print.append(printLine(y))
     }
     print.toString
   }
 
-  private def printLine(print: StringBuilder, y: Int) = {
-    printExternalSpaces(print, y)
+  private def printLine(y: Int) = {
+    val print = newBuilder
+    print.append(printExternalSpaces(y))
 
     print.append("*")
 
@@ -25,14 +28,16 @@ case class Diamond(size: Int) {
       print.append("*")
     }
 
-    printExternalSpaces(print, y)
+    print.append(printExternalSpaces(y))
 
     print.append("\n")
   }
 
-  private def printExternalSpaces(print: StringBuilder, y: Int) = {
+  private def printExternalSpaces(y: Int) = {
+    val print = newBuilder
     for (x <- 0 until y) {
       print.append(" ")
     }
+    print.toString
   }
 }
