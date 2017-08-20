080d932aa7ea1d4e34ac25566bea2e815fc78ede
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2017-08-20 15:41:34 +0200

    adventofcode: fix build

--- adventofcode/scala/src/main/scala/name/lemerdy/sebastian/katas/md5/AdventCoinsMining.scala
+++ adventofcode/scala/src/main/scala/name/lemerdy/sebastian/katas/md5/AdventCoinsMining.scala
@@ -5,10 +5,11 @@ import java.security.MessageDigest
 class AdventCoinsMining {
 
   def mine(secretKey: String, numberOfZeros: Int = 5): Int = {
+    val zeros = (0 until numberOfZeros).map(_ => "0").mkString
     val hasher: MessageDigest = MessageDigest.getInstance("MD5")
     var hash = "123456"
     var i = -1
-    while (!hash.substring(0, 6).equals("000000")) {
+    while (!hash.substring(0, numberOfZeros).equals(zeros)) {
       i += 1
       hash = hasher.digest(s"$secretKey$i".getBytes()).map("%02X".format(_)).mkString
       if (i % 1000 == 0) print(s"${i / 1000} ")
