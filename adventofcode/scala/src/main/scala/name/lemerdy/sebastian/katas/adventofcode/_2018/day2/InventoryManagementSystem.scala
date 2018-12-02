efda0cbe9f4b3ecf79a5be7af27ff17b58232447
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2018-12-02 18:51:10 +0100

    adventofcode: resolve day 2 Inventory Mngmt System

---
+++ adventofcode/scala/src/main/scala/name/lemerdy/sebastian/katas/adventofcode/_2018/day2/InventoryManagementSystem.scala
@@ -0,0 +1,44 @@
+package name.lemerdy.sebastian.katas.adventofcode._2018.day2
+
+import scala.annotation.tailrec
+
+object InventoryManagementSystem {
+
+  def checksum(boxIDs: Iterator[String]): BigInt = {
+    val (twoOccurrences, threeOccurences) = countLetters(boxIDs.toSeq)
+    twoOccurrences * threeOccurences
+  }
+
+  def commonLettersBetweenTwoCorrectBoxIDs(boxIDs: Iterator[String]): String =
+    commonLettersBetweenTwoCorrectBoxIDs(boxIDs.toList)
+
+  @tailrec
+  private def countLetters(boxIDs: Seq[String],
+                           twoLetters: Int = 0,
+                           threeLetters: Int = 0): (Int, Int) = boxIDs match {
+    case Nil ⇒ (twoLetters, threeLetters)
+    case boxID #:: tail ⇒ countLetters(
+      boxIDs = tail,
+      twoLetters = twoLetters + booleanToInt(hasLetterOfExactlyNumber(boxID, 2)),
+      threeLetters = threeLetters + booleanToInt(hasLetterOfExactlyNumber(boxID, 3)))
+  }
+
+  private def hasLetterOfExactlyNumber(boxID: String, exactly: Int): Boolean =
+    boxID.groupBy(identity).values.map(_.length).exists(_ == exactly)
+
+  private def booleanToInt(value: Boolean): Int = if (value) 1 else 0
+
+  private def commonLettersBetweenTwoCorrectBoxIDs(boxIDs: List[String]): String = boxIDs match {
+    case Nil ⇒ ""
+    case boxID :: tail ⇒ tail.flatMap(sameLettersIfOnlyOneLetterIsDifferent(boxID))
+      .headOption
+      .getOrElse(commonLettersBetweenTwoCorrectBoxIDs(tail))
+  }
+
+  private def sameLettersIfOnlyOneLetterIsDifferent(firstBoxID: String)(secondBoxID: String): Option[String] =
+    firstBoxID.zip(secondBoxID).partition { case (firstLetter, secondLetter) ⇒ firstLetter == secondLetter } match {
+      case (same, different) if different.length == 1 ⇒ Some(same.map(_._1).mkString(""))
+      case _ ⇒ None
+    }
+
+}
---
+++ adventofcode/scala/src/test/scala/name/lemerdy/sebastian/katas/adventofcode/_2018/day2/InventoryManagementSystemSpec.scala
@@ -0,0 +1,59 @@
+package name.lemerdy.sebastian.katas.adventofcode._2018.day2
+
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.io.Source
+
+class InventoryManagementSystemSpec extends FlatSpec with Matchers {
+
+  "InventoryManagementSystem" should "compute checksum" in {
+    val boxIDs: Iterator[String] = Iterator(
+      "abcdef",
+      "bababc",
+      "abbcde",
+      "abcccd",
+      "aabcdd",
+      "abcdee",
+      "ababab",
+    )
+
+    val checksum: BigInt = InventoryManagementSystem.checksum(boxIDs)
+
+    checksum should be(BigInt(12))
+  }
+
+  it should "compute checksum for dataset" in {
+    val boxIDs: Iterator[String] = Source.fromResource(dataset).getLines()
+
+    val checksum: BigInt = InventoryManagementSystem.checksum(boxIDs)
+
+    checksum should be(BigInt(6422))
+  }
+
+  it should "find letters that are common between the two correct box IDs" in {
+    val boxIDs: Iterator[String] = Iterator(
+      "abcde",
+      "fghij",
+      "klmno",
+      "pqrst",
+      "fguij",
+      "axcye",
+      "wvxyz",
+    )
+
+    val commonLettersBetweenTwoCorrectBoxIDs: String = InventoryManagementSystem.commonLettersBetweenTwoCorrectBoxIDs(boxIDs)
+
+    commonLettersBetweenTwoCorrectBoxIDs should be("fgij")
+  }
+
+  it should "find letters that are common between the two correct box IDs for dataset" in {
+    val boxIDs: Iterator[String] = Source.fromResource(dataset).getLines()
+
+    val commonLettersBetweenTwoCorrectBoxIDs: String = InventoryManagementSystem.commonLettersBetweenTwoCorrectBoxIDs(boxIDs)
+
+    commonLettersBetweenTwoCorrectBoxIDs should be("qcslyvphgkrmdawljuefotxbh")
+  }
+
+  private lazy val dataset = "name/lemerdy/sebastian/katas/adventofcode/_2018/day2/dataset.txt"
+
+}
