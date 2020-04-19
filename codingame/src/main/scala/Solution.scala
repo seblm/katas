e1832a00dbcf4b99d673e569b85d730a5d09ba63
Sébastian Le Merdy <567637+seblm@users.noreply.github.com>
2020-04-19 22:29:31 +0200

    codingame: empty Solution template for Clash of Code

---
+++ codingame/src/main/scala/Solution.scala
@@ -0,0 +1,10 @@
+import math._
+import scala.util._
+import scala.io.StdIn._
+
+object Solution extends App {
+
+  // Write an answer using println
+  // To debug: Console.err.println("Debug messages...")
+
+}
