3f25d4a2e3f9816d780c943794ac1ee4583b6504
Sébastian Le Merdy <567637+seblm@users.noreply.github.com>
2020-12-02 02:06:10 +0100

    adventofcode: 2020 day 1

---
+++ adventofcode/scala/src/main/scala/name/lemerdy/sebastian/katas/adventofcode/_2020/day1/ExpenseReportFixer.scala
@@ -0,0 +1,25 @@
+package name.lemerdy.sebastian.katas.adventofcode._2020.day1
+
+import scala.annotation.tailrec
+
+object ExpenseReportFixer {
+
+  def product2Numbers(expenseReport: List[Long], sum: Long = 2020): Either[String, Long] = expenseReport match {
+    case Nil => Left("expense report can't be empty")
+    case head :: tail => productRecursive(sum, head, tail).orElse(product2Numbers(tail, sum))
+  }
+
+  def product3Numbers(expenseReport: List[Long], sum: Long = 2020): Either[String, Long] = expenseReport match {
+    case Nil => Left("expense report can't be empty")
+    case first :: Nil => Left(s"expense report can't have only one value $first")
+    case first :: tail => product2Numbers(tail, sum - first).map(_ * first).orElse(product3Numbers(tail, sum))
+  }
+
+  @tailrec
+  private def productRecursive(sum: Long, fixed: Long, numbers: List[Long]): Either[String, Long] = numbers match {
+    case head :: _ if fixed + head == sum => Right(fixed * head)
+    case _ :: tail => productRecursive(sum, fixed, tail)
+    case Nil => Left(s"there is no other number which $fixed + x = $sum")
+  }
+
+}
---
+++ adventofcode/scala/src/test/scala/name/lemerdy/sebastian/katas/adventofcode/_2020/day1/ExpenseReportFixerSpec.scala
@@ -0,0 +1,46 @@
+package name.lemerdy.sebastian.katas.adventofcode._2020.day1
+
+import org.scalatest.OptionValues._
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers._
+
+import scala.io.Source
+import scala.util.Try
+
+class ExpenseReportFixerSpec extends AnyFlatSpec {
+
+  lazy val input: List[Long] = Source.fromResource("name/lemerdy/sebastian/katas/adventofcode/_2020/day1/input.txt").getLines().flatMap(line => Try(line.toLong).toOption).toList
+
+  "ExpenseReportFixer" should "compute 2 numbers product that sum is 2020" in {
+    val expenseReport: List[Long] = List(1721, 979, 366, 299, 675, 1456)
+
+    val result = ExpenseReportFixer.product2Numbers(expenseReport)
+
+    result.toOption.value shouldBe 514579
+  }
+
+  it should "compute 2 numbers product with real example" in {
+    val expenseReport: List[Long] = input
+
+    val result = ExpenseReportFixer.product2Numbers(expenseReport)
+
+    result.toOption.value shouldBe 913824
+  }
+
+  it should "compute 3 numbers product that sum is 2020" in {
+    val expenseReport: List[Long] = List(1721, 979, 366, 299, 675, 1456)
+
+    val result = ExpenseReportFixer.product3Numbers(expenseReport)
+
+    result.toOption.value shouldBe 241861950
+  }
+
+  it should "compute 3 numbers product with real example" in {
+    val expenseReport: List[Long] = input
+
+    val result = ExpenseReportFixer.product3Numbers(expenseReport)
+
+    result.toOption.value shouldBe 240889536
+  }
+
+}
