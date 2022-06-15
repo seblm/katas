75af6d4783ef24671345c668774e7607e4102ec9
Sébastian Le Merdy <567637+seblm@users.noreply.github.com>
2022-06-15 05:16:42 +0200

    function-reuse: add affordable function which handle Option

--- function-reuse/src/main/scala/kata/function/reuse/Item.scala
+++ function-reuse/src/main/scala/kata/function/reuse/Item.scala
@@ -11,4 +11,6 @@ case object Item:
 
   def affordable(item: Item): Boolean = item.price < 500
 
-  def affordable(oitem: Option[Item]): Option[Boolean] = ???
+  def affordable(oitem: Option[Item]): Option[Boolean] = oitem match
+    case Some(item) => Some(affordable(item))
+    case None       => None
