09a0b3b2b813f4109dee6336abde4099bac487ec
Sébastian Le Merdy <567637+seblm@users.noreply.github.com>
2022-08-12 09:11:26 +0200

    castaway-island: add some usefull functions

--- castaway-island/src/main/scala/CastawayIsland.scala
+++ castaway-island/src/main/scala/CastawayIsland.scala
@@ -1,5 +1,33 @@
 import scala.util.Random
 
+class CastawayIsland1(state: CastawayIsland = CastawayIsland(Random())):
+
+  def currentPlayerCards: Vector[Card] = state.players(state.currentPlayerIndex).cards
+
+  def put(card: Card): CastawayIsland1 =
+    CastawayIsland1(
+      state.copy(
+        players = state.players.zipWithIndex.map {
+          case (currentPlayer, index) if index == state.currentPlayerIndex =>
+            currentPlayer.copy(currentPlayer.cards.filter(_ == card))
+          case (otherPlayer, _) => otherPlayer
+        },
+        islands = state.islands.zipWithIndex.map {
+          case (currentPlayerIsland, index) if index == state.currentPlayerIndex =>
+            currentPlayerIsland.copy(currentPlayerIsland.resources.map {
+              case (resource, cards) if resource == card.resource => resource -> cards.appended(card)
+              case (resource, cards)                              => resource -> cards
+            })
+          case (otherIsland, _) =>
+            otherIsland
+        },
+        currentPlayerIndex = state.currentPlayerIndex + 1 % 4
+      )
+    )
+
+  def kind(islandIndex: Int, resource: Resource): Kind =
+    state.islands(islandIndex).resources(resource).last.kind
+
 case class CastawayIsland(
     players: Vector[Player],
     islands: Vector[Island],
@@ -14,7 +42,7 @@ object CastawayIsland:
 
   def apply(seed: Long): CastawayIsland = CastawayIsland(Random(seed))
 
-  private def apply(random: Random): CastawayIsland =
+  def apply(random: Random): CastawayIsland =
     val deck = random.shuffle(for {
       resource <- Resource.all
       kind <- Kind.all
--- castaway-island/src/test/scala/CastawayIslandTest.scala
+++ castaway-island/src/test/scala/CastawayIslandTest.scala
@@ -1,6 +1,37 @@
+import Kind.{Desastre, ResourceKind, Riposte}
+import Resource.{Betail, Bois, Eau, Feu}
 import munit.FunSuite
 
+import scala.util.Random
+
 class CastawayIslandTest extends FunSuite:
-  test("CastawayIsland should be created") {
-    val castawayIsland = CastawayIsland()
+
+  test("current player should have 6 cards into his hands") {
+    val castawayIsland = CastawayIsland1()
+
+    assertEquals(castawayIsland.currentPlayerCards.length, 6)
+  }
+
+  test("current player cards should be some values") {
+    val castawayIsland = CastawayIsland1(CastawayIsland(Random(1)))
+
+    assertEquals(
+      castawayIsland.currentPlayerCards,
+      Vector(
+        Card(Betail, Desastre),
+        Card(Bois, ResourceKind),
+        Card(Eau, ResourceKind),
+        Card(Bois, Riposte),
+        Card(Bois, Desastre),
+        Card(Feu, Riposte)
+      )
+    )
+  }
+
+  test("current player should put a resource into board") {
+    val castawayIsland = CastawayIsland1(CastawayIsland(Random(1)))
+
+    val nextCastawayIsland = castawayIsland.put(Card(Bois, ResourceKind))
+
+    assertEquals(nextCastawayIsland.kind(islandIndex = 0, resource = Bois), ResourceKind)
   }
