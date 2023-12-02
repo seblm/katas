2e8190a7e854911ed2a104ad6f8059a6512d7be2
Sébastian Le Merdy <567637+seblm@users.noreply.github.com>
2023-12-02 22:04:46 +0100

    adventofcode: 2023 day 02

--- adventofcode/src/main/scala/name/lemerdy/sebastian/katas/adventofcode/_2023/day02/CubeConundrum.scala
+++ adventofcode/src/main/scala/name/lemerdy/sebastian/katas/adventofcode/_2023/day02/CubeConundrum.scala
@@ -1,3 +1,35 @@
-package name.lemerdy.sebastian.katas.adventofcode._2023.day02 object CubeConundrum {
+package name.lemerdy.sebastian.katas.adventofcode._2023.day02
 
-}
+import scala.math.max
+
+object CubeConundrum:
+  private case class Bag(red: Int, green: Int, blue: Int):
+    def isPossible: Boolean = red <= 12 && green <= 13 && blue <= 14
+    def power: Int = red * green * blue
+  private object Bag:
+    private val regex = """(\d+) (.+)""".r
+    def apply(bags: String): Bag =
+      bags
+        .split(',')
+        .map(_.trim)
+        .map(colorToBag)
+        .fold(Bag(0, 0, 0))((b1, b2) => Bag(b1.red + b2.red, b1.green + b2.green, b1.blue + b2.blue))
+    private def colorToBag(color: String): Bag = color match
+      case regex(count, "red")   => Bag(count.toInt, 0, 0)
+      case regex(count, "green") => Bag(0, count.toInt, 0)
+      case regex(count, "blue")  => Bag(0, 0, count.toInt)
+  private case class Game(id: Int, bags: Seq[Bag]):
+    private def maxBag: Bag = bags
+      .fold(Bag(0, 0, 0))((b1, b2) => Bag(max(b1.red, b2.red), max(b1.green, b2.green), max(b1.blue, b2.blue)))
+    def isPossible: Boolean = maxBag.isPossible
+    def power: Int = maxBag.power
+  private object Game:
+    private val regex = """Game (\d+): (.+)""".r
+    def apply(line: String): Game = line match
+      case regex(id, bags) => Game(id.toInt, bags.split(';').map(Bag.apply))
+
+  private def games(input: String): Seq[Game] = input.split('\n').map(_.trim).map(Game.apply)
+
+  def sum(input: String): Int = games(input).filter(_.isPossible).map(_.id).sum
+
+  def power(input: String): Int = games(input).map(_.power).sum
--- adventofcode/src/test/scala/name/lemerdy/sebastian/katas/adventofcode/_2023/day02/CubeConundrumSpec.scala
+++ adventofcode/src/test/scala/name/lemerdy/sebastian/katas/adventofcode/_2023/day02/CubeConundrumSpec.scala
@@ -1,3 +1,42 @@
-package name.lemerdy.sebastian.katas.adventofcode._2023.day02 class CubeConundrumSpec {
+package name.lemerdy.sebastian.katas.adventofcode._2023.day02
 
-}
+import org.scalatest.TryValues.given
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers.*
+
+import scala.io.Source
+import scala.util.Using
+
+class CubeConundrumSpec extends AnyFlatSpec:
+  "Cube Conundrum" should "sum IDs of possible games" in:
+    val sum = CubeConundrum.sum("""Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+                                  |Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+                                  |Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+                                  |Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+                                  |Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green""".stripMargin)
+
+    sum should be(8)
+
+  private lazy val input =
+    Using(Source.fromResource("name/lemerdy/sebastian/katas/adventofcode/_2023/day02/input.txt"))(
+      _.mkString
+    ).success.value
+
+  it should "sum IDs of possible games with real example" in:
+    val sum = CubeConundrum.sum(input)
+
+    sum should be(2_265)
+
+  it should "compute power" in:
+    val power = CubeConundrum.power("""Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+                                      |Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+                                      |Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+                                      |Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+                                      |Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green""".stripMargin)
+
+    power should be(2_286)
+
+  it should "compute power with real example" in:
+    val power = CubeConundrum.power(input)
+
+    power should be(64_097)
