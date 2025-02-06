6221b3901dce58038a449111aa0cba904a9241a4
FlorianTran <florian.tranpro@gmail.com>
2025-02-06 14:47:55 +0100

    fix

--- tondeuse-scala/src/main/scala/Main.scala
+++ tondeuse-scala/src/main/scala/Main.scala
@@ -7,3 +7,19 @@ object Main {
     Server.start(8080)
   }
 }
+
+/* 
+* Exemple de requête HTTP POST:*
+
+curl -X POST -H "Content-Type: application/json" -d '{                                   
+  "width":6,
+  "height":6,
+  "mowers":[
+    {"position":"1 2 N", "instructions":"GAGAGAGAA"},
+    {"position":"3 3 E", "instructions":"AADAADADDA"}
+  ]
+}' http://localhost:8080/
+
+* Réponse attendue:*
+{"finalPositions":["1 3 N","5 1 E"]}
+ */
